c3ce6e922bb71dae34c5735dfc0edff60e00433d
travis carney <traviscarney@gmail.com>
2019-06-27 06:17:00 -0400

    Updated Scroll Function
    Updated on Scroll function to only fire on proper screen 768 and less.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+/*var windowWidth = window.outerWidth;
+function x() {
+  if (windowWidth > 768)
+    return 1;
+  else
+    window.onscroll = function() {
+	var currentScrollPos = window.pageYOffset;
+  		if (prevScrollpos > currentScrollPos) {
+    	document.getElementById("navbar").style.bottom = "0px";
+  		} else {
+    	document.getElementById("navbar").style.bottom = "-50px";
+  		}
+  		prevScrollpos = currentScrollPos;
+	}
+}*/
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+var windowWidth = window.outerWidth;
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (prevScrollpos > currentScrollPos && windowWidth <= 768) {
     document.getElementById("navbar").style.bottom = "0px";
   } else {
     document.getElementById("navbar").style.bottom = "-50px";
